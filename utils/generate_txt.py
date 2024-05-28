492f09bb8f943512eda6bfe0dc5b632296124204
Noah Lin <noah@c-link.com.tw>
2024-05-28 10:49:05 +0800

    remove aug from generate_txt

--- utils/generate_txt.py
+++ utils/generate_txt.py
@@ -12,31 +12,18 @@ def generate_txt(opts):
     with open(train_txt, "w") as train_txt:
         for img in sorted(Path(train_dir).glob("**/*.[jJpP][pPnN][gG]")):
             img_path = Path(img).resolve()
-            if not opts.aug:
-                if not any(
-                    substr in str(img) for substr in ["_cot", "_brt", "_ivt", "_sat"]
-                ):
-                    train_txt.write(str(img_path) + "\n")
-            else:
-                train_txt.write(str(img_path) + "\n")
+            train_txt.write(str(img_path) + "\n")
 
     # val dir
     with open(val_txt, "w") as val_txt:
         for img in sorted(Path(val_dir).glob("**/*.[jJpP][pPnN][gG]")):
             img_path = Path(img).resolve()
-            if not opts.aug:
-                if not any(
-                    substr in str(img) for substr in ["_cot", "_brt", "_ivt", "_sat"]
-                ):
-                    val_txt.write(str(img_path) + "\n")
-            else:
-                val_txt.write(str(img_path) + "\n")
+            val_txt.write(str(img_path) + "\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("train_dir", help="training dataset directory")
     parser.add_argument("val_dir", help="validation dataset directory")
-    parser.add_argument("--aug", action="store_true", help="augmentation flag")
     args = parser.parse_args()
     generate_txt(args)
